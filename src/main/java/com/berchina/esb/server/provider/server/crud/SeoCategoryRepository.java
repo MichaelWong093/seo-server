49ea207c4debb609438f21bc08cee8a27681bbf4
rxbyes <rxbyes@gmail.com>
2016-11-04 09:45:01 +0800

    代码优化

--- src/main/java/com/berchina/esb/server/provider/server/crud/SeoCategoryRepository.java
+++ src/main/java/com/berchina/esb/server/provider/server/crud/SeoCategoryRepository.java
@@ -1,7 +1,5 @@
 package com.berchina.esb.server.provider.server.crud;
 
-import com.alibaba.fastjson.JSON;
-import com.berchina.esb.server.configloader.config.SolrServerFactoryBean;
 import com.berchina.esb.server.provider.client.SeoRequest;
 import com.berchina.esb.server.provider.model.SeoCateGory;
 import com.berchina.esb.server.provider.model.SeoGoods;
@@ -9,16 +7,13 @@ import com.berchina.esb.server.provider.utils.*;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
-import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.client.solrj.response.FacetField;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
---
+++ src/test/java/com/seo/server/category/CategoryServerTests.java
@@ -0,0 +1,24 @@
+package com.seo.server.category;
+
+import com.berchina.esb.server.SeoApplication;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ * @Package com.seo.server.category
+ * @Description: TODO ( 类目搜索测试 )
+ * @Author 任小斌 renxiaobin
+ * @Date 2016 上午9:30
+ * @Version V1.0
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = SeoApplication.class)
+@WebAppConfiguration
+public class CategoryServerTests {
+
+
+
+}
