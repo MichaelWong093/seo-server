55754005d860bbd9f2eff78fa2dd270e145091fb
rxbyes <rxbyes@gmail.com>
2017-04-06 20:00:45 +0800

    项目优化

--- src/main/java/com/berchina/seo/server/SeoApplication.java
+++ src/main/java/com/berchina/seo/server/SeoApplication.java
@@ -13,7 +13,6 @@ import org.springframework.web.WebApplicationInitializer;
  * @Date 15/12/1 下午1:14
  * @Version V1.0
  */
-
 @SpringBootApplication
 public class SeoApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
 
